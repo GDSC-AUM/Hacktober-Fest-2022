cfa0adf353b30351dd7a4686c3977014c45481e7
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-10-17 21:46:16 +0530

    Added Optimizing Algorithms

---
+++ CPP/buy_and_sell_type1.cpp
@@ -0,0 +1,35 @@
+/*
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+  */
+
+ int maxProfit(vector<int>& nums) {
+        int n = nums.size();
+        
+        int mini = nums[0];
+        int max_value = INT_MIN;
+        int curr =0;
+        for(int i=0;i<n;i++){
+            mini = min(mini,nums[i]);
+            
+            max_value = max(max_value,nums[i]-mini);
+  
+        }
+        
+return max_value;        
+    }
